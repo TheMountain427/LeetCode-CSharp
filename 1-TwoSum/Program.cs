7761eb828b6be31136acca2479ad11c298f5d49a
TheMountain427 <166150577+TheMountain427@users.noreply.github.com>
2024-06-28 16:21:50 -0500

    Add files via upload

---
+++ 1-TwoSum/Program.cs
@@ -0,0 +1,34 @@
+﻿
+public class Solution {
+    public int[]? Output;
+    public int[] TwoSum(int[] nums, int target) 
+    {
+        var Target = target;
+        var Nums = nums;
+
+        for (int i = 0; i <= (Nums.Length - 1); i++)
+        {
+            for (int x = 1; x <= (Nums.Length - 1); x++)
+            {
+                if (i != x && (Nums[i] + Nums[x]) == Target)
+                {
+                    Output = [i,x];
+                    return Output;
+                }
+            }
+        }
+        Output = [0,0];
+        return Output;
+
+    }
+    static void Main(string[] args)
+    {
+        var sol = new Solution();
+        var h = sol.TwoSum([2,5,5,11], 10);
+        foreach (int item in h)
+        {
+            Console.WriteLine($"{item}");
+        }
+        
+    }
+}
---
+++ 9-Palindrome Number/Program.cs
@@ -0,0 +1,58 @@
+﻿
+
+public class Solution {
+    public bool IsPalindrome(int x) {
+
+        if (x < 0)
+        {
+            return false;
+        }
+
+        var splitter = 10;
+
+        if ( x < splitter)
+        { 
+            return true;
+        }
+
+        else
+        {
+            var xsplit = new List<int>();
+            var remainder = x % splitter;
+            xsplit.Add(remainder);
+            
+            var lead = x / splitter;
+
+            if (lead < 10)
+            {
+                xsplit.Add(remainder);
+            }
+
+            while (lead > 9)
+            {
+                remainder = lead % splitter;
+                xsplit.Add(remainder);
+
+                lead = lead / splitter;
+            }
+
+            xsplit.Add(lead);
+            
+            for (int i = 0, c = xsplit.Count - 1; i < c; i++, c--)
+            {
+                if (xsplit[i] != xsplit[c])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+        static void Main(string[] args)
+    {
+        var output = new Solution();
+        var h = output.IsPalindrome(1001);
+        Console.WriteLine($"{h}");
+    }
+}
+
