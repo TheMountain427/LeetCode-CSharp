6f1e4fb567353e2c0b8a02a5e6cfda0401dab473
TheMountain427 <166150577+TheMountain427@users.noreply.github.com>
2024-07-03 16:47:15 -0500

    Add files via upload

---
+++ 12-Roman to Integer/Program.cs
@@ -0,0 +1,37 @@
+﻿// I = 1, Max III
+//  -> IV = 4/ IX = 9
+// V = 5
+// X = 10 -> XL = 40/ XC = 90
+// L = 50
+// C = 100 -> CD = 400/ CM = 900
+// D = 500
+// M = 1000
+
+// 
+
+// I    ->  X    ->  C
+// II   ->  XX   ->  CC
+// III  ->  XXX  ->  CCC  
+// IV   ->  XL   ->  CD
+// V    ->  L    ->  D
+// VI   ->  LX   ->  DC
+// VII  ->  LXX  ->  DCC
+// VIII ->  LXXX ->  DCCC
+// IX   ->  XC   ->  CM
+// X    ->  C    ->  M
+namespace RomantoInteger;
+public class Solution 
+{
+    public int RomanToInt(string s) 
+    {
+        return 0;
+    }
+    static void Main()
+    {
+
+
+    }
+
+
+}
+
